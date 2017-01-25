a52a61e59222d9e50a367c4a65cd014b6659cf2c
vadim0107 <vadimkuznetsov96@gmail.com>
2017-01-25 11:21:12 +0200

    add section about-us

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOltdfQ==
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:60,
+    navText: [],
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
